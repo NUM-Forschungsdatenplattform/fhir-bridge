30fce37dd8b8d4c368f932d4f33a8463379285ed
José Pedro Correia <jose.pedro.correia.ext@vitagroup.ag>
2022-10-25 10:45:31 +0100

    Make using testcontainers optional

--- src/test/java/org/ehrbase/fhirbridge/AbstractIntegrationTest.java
+++ src/test/java/org/ehrbase/fhirbridge/AbstractIntegrationTest.java
@@ -15,50 +15,59 @@ public class AbstractIntegrationTest {
 
     private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(AbstractIntegrationTest.class);
 
-    private static final int EHRBASE_SERVICE_PORT = 8080;
+    private static GenericContainer<?> ehrbaseContainer;
 
-    private static final GenericContainer<?> ehrbaseContainer;
+    private static boolean useTestContainers = true;
 
     static {
-        Network ehrbaseNetwork = Network.newNetwork();
+        String useTestContainersEnv = System.getenv("USETESTCONTAINERS");
+        if (useTestContainersEnv != null) {
+            useTestContainers = Boolean.parseBoolean(useTestContainersEnv);
+        }
 
-        GenericContainer<?> ehrbaseDatabaseContainer =
-                new GenericContainer<>(DockerImageName.parse("ehrbase/ehrbase-postgres:13.4"))
-                        .withNetwork(ehrbaseNetwork)
-                        .withNetworkAliases("ehrbase-database")
-                        .withEnv("POSTGRES_USER", "postgres")
-                        .withEnv("POSTGRES_PASSWORD", "postgres")
-                        .withEnv("EHRBASE_USER", "ehrbase")
-                        .withEnv("EHRBASE_PASSWORD", "ehrbase")
-                        .waitingFor(Wait.forLogMessage(".*database system is ready to accept connections.*", 2));
+        if (useTestContainers) {
+            Network ehrbaseNetwork = Network.newNetwork();
 
-        log.info("Starting EHRbase database...");
-        ehrbaseDatabaseContainer.start();
-        log.info("EHRbase database started.");
+            GenericContainer<?> ehrbaseDatabaseContainer =
+                    new GenericContainer<>(DockerImageName.parse("ehrbase/ehrbase-postgres:13.4"))
+                            .withNetwork(ehrbaseNetwork)
+                            .withNetworkAliases("ehrbase-database")
+                            .withEnv("POSTGRES_USER", "postgres")
+                            .withEnv("POSTGRES_PASSWORD", "postgres")
+                            .withEnv("EHRBASE_USER", "ehrbase")
+                            .withEnv("EHRBASE_PASSWORD", "ehrbase")
+                            .waitingFor(Wait.forLogMessage(".*database system is ready to accept connections.*", 2));
 
-        ehrbaseContainer = new GenericContainer<>(DockerImageName.parse("ehrbase/ehrbase:0.17.2"))
-                .dependsOn(ehrbaseDatabaseContainer)
-                .withNetwork(ehrbaseNetwork)
-                .withEnv("DB_URL", "jdbc:postgresql://ehrbase-database:5432/ehrbase")
-                .withEnv("DB_USER", "ehrbase")
-                .withEnv("DB_PASS", "ehrbase")
-                .withEnv("AUTH_TYPE", "BASIC")
-                .withEnv("AUTH_USER", "ehrbase-user")
-                .withEnv("AUTH_PASSWORD", "SuperSecretPassword")
-                .withEnv("SYSTEM_NAME", "local.ehrbase.org")
-                .withExposedPorts(EHRBASE_SERVICE_PORT)
-                .waitingFor(Wait.forLogMessage(".*Started EhrBase in.*", 1));
+            log.info("Starting EHRbase database...");
+            ehrbaseDatabaseContainer.start();
+            log.info("EHRbase database started.");
 
-        log.info("Starting EHRbase...");
-        ehrbaseContainer.start();
-        log.info("EHRbase started.");
+            ehrbaseContainer = new GenericContainer<>(DockerImageName.parse("ehrbase/ehrbase:0.17.2"))
+                    .dependsOn(ehrbaseDatabaseContainer)
+                    .withNetwork(ehrbaseNetwork)
+                    .withEnv("DB_URL", "jdbc:postgresql://ehrbase-database:5432/ehrbase")
+                    .withEnv("DB_USER", "ehrbase")
+                    .withEnv("DB_PASS", "ehrbase")
+                    .withEnv("AUTH_TYPE", "BASIC")
+                    .withEnv("AUTH_USER", "ehrbase-user")
+                    .withEnv("AUTH_PASSWORD", "SuperSecretPassword")
+                    .withEnv("SYSTEM_NAME", "local.ehrbase.org")
+                    .withExposedPorts(8080)
+                    .waitingFor(Wait.forLogMessage(".*Started EhrBase in.*", 1));
+
+            log.info("Starting EHRbase...");
+            ehrbaseContainer.start();
+            log.info("EHRbase started.");
+        }
     }
 
     @DynamicPropertySource
     private static void setProperties(DynamicPropertyRegistry registry) {
-        registry.add("ehrbase.address", ehrbaseContainer::getHost);
-        registry.add("ehrbase.port", () -> ehrbaseContainer.getMappedPort(EHRBASE_SERVICE_PORT));
-        registry.add("ehrbase.path", () -> "/ehrbase/rest/openehr/v1/");
+        if (useTestContainers) {
+            registry.add("ehrbase.address", ehrbaseContainer::getHost);
+            registry.add("ehrbase.port", () -> ehrbaseContainer.getMappedPort(8080));
+            registry.add("ehrbase.path", () -> "/ehrbase/rest/openehr/v1/");
+        }
     }
 
 }
